3c2e3ee7d2a9fed5dc220c81c37a9f688134c8fc
Tano Fabien <kobebroullon@gmail.com>
2025-12-04 21:08:05 +0000

    Début de la configuration du template

---
+++ src/Controller/TableaudebordController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/')]
+final class TableaudebordController extends AbstractController
+{
+    #[Route('', name: 'app_tableaudebord')]
+    public function index(): Response
+    {
+        return $this->render('tableaudebord/index.html.twig', [
+            'controller_name' => 'TableaudebordController',
+        ]);
+    }
+}
